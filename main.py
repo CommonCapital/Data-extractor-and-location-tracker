d477bf8c8160fd56d4b9892761c2037bdf9a28fd
CommonCapital <nursan2007@gmail.com>
2025-08-27 04:16:52 +0500

    commit everything

---
+++ main.py
@@ -0,0 +1,41 @@
+import phonenumbers
+from phone import number
+import opencage
+from phonenumbers import geocoder
+import folium
+import os
+
+geocoder = OpenCageGeocode(os.getenv('API_KEY'))
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+
+from phonenumbers import carrier
+
+service_pro = phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, "en"))
+
+
+
+from opencage.geocoder import OpenCageGeocode
+
+
+geocoder = OpenCageGeocode(os.getenv('API_KEY'))
+
+query = str(location)
+results = geocoder.geocode(query)
+print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+print(lat, lng)
+
+myMap = folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to(myMap)
+
+myMap.save("location.html")
+
+
+
